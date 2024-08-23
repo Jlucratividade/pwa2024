db123e13be95ca7066b6dfcb787f3b597fad6409
Jlucratividade <95404301+Jlucratividade@users.noreply.github.com>
2024-08-22 23:11:46 -0300

    comit01

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'ola-mundo-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/scripts.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
